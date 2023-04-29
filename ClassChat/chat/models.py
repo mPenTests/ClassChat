5f5fdbf97c00257a0daa55aaf6acbae9491e9160
mPenTests <marko.prifti4@gmail.com>
2023-04-29 20:59:38 +0200

    Fixed security risk

--- ClassChat/chat/models.py
+++ ClassChat/chat/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+    
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
+    messages = models.ManyToManyField(Message)
+
+    def __str__(self):
+        return self.name
