db3f745dfd89416d9bb21db86e83232b79e77cbf
mPenTests <marko.prifti4@gmail.com>
2023-04-29 22:05:32 +0200

    Added id on Message

--- ClassChat/chat/models.py
+++ ClassChat/chat/models.py
@@ -7,6 +7,7 @@ class User(AbstractUser):
 
 
 class Message(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
