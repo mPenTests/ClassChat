c91655010d7a398acd8a61d55bdc448dfd232c9b
mPenTests <marko.prifti4@gmail.com>
2023-05-01 11:58:20 +0200

    Added Icons

--- chatUI/src/components/Auth.jsx
+++ chatUI/src/components/Auth.jsx
@@ -1,30 +1,35 @@
-import '../styles/auth.scss'
-import Button from 'react-bootstrap/Button';
+import '../styles/auth.scss';
 import Form from 'react-bootstrap/Form';
+import { Flex } from '@chakra-ui/react';
+import { FcGoogle } from 'react-icons/fc';
+import { AiFillApple } from 'react-icons/ai';
+
 
 function Auth() {
     return (
         <div className="auth-container">
             <div className="auth-box">
                 <Form>
+                    <h3 id="formTitle">Login to MarkNet</h3>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control type="email" placeholder="Enter email" />
+                      <Form.Label>Username</Form.Label>
+                      <Form.Control required type="email" placeholder="Member Name" />
                       <Form.Text className="text-muted">
-                            We&apos;ll never share your email with anyone else.
+                            Greetings from Marko!
                       </Form.Text>
                     </Form.Group>
-
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                       <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" />
+                      <Form.Control required type="password" placeholder="Password" />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                      <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                      Submit
-                    </Button>
+                    <Flex flexDirection={"column"} gap={"20px"} id='auth-flex'>
+                        <button className="auth-btn" type="submit">Login</button>
+                        <button className="auth-btn" type="submit">Register</button>
+                    </Flex>
+                    <Flex flexDirection={"row"} justifyContent={"center"} id='oauth-flex' gap={"20px"}>
+                        <button><FcGoogle /></button>
+                        <button><AiFillApple /></button>
+                    </Flex>
                 </Form>
             </div>
         </div>
